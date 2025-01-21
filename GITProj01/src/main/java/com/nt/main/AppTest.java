57e6dcb3069ec95634f2f96a9f17e195ad135eec
shire <shire@thinkingpad>
2025-01-21 13:55:49 -0500

    Task 1 from dev1

--- GITProj01/src/main/java/com/nt/main/AppTest.java
+++ GITProj01/src/main/java/com/nt/main/AppTest.java
@@ -1,8 +1,12 @@
 package com.nt.main;
 
+import com.nt.service.ArithematicOperations;
+
 public class AppTest {
 	public static void main(String[] args) {
-		
+		ArithematicOperations ar=new ArithematicOperations();
+		System.out.println("Sum: "+ar.sum(100,200));
+		System.out.println("sub: "+ar.sub(400,200));
 	}
 
 }
